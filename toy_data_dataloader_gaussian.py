14b0135dea26665fc79745287f8f35930ee880af
valentin.dorseuil@gmail.com <valentin.dorseuil@gmail.com>
2024-03-05 15:03:32 +0100

    Valou

--- toy_data_dataloader_gaussian.py
+++ toy_data_dataloader_gaussian.py
@@ -14,13 +14,15 @@ def get_dataloader(d = 2, r = 100, N = 500, batch_size = 50):
 
     # simulate the gaussian
     X = torch.tensor(stats.norm.rvs(loc=0, scale=1, size=(N, r, d)), dtype=torch.float32)
-    locs = torch.randint(-10, 10, (N,), dtype=torch.float32).repeat_interleave(r).view(N, r)
-    scales = 5*torch.rand(N, dtype=torch.float32).repeat_interleave(r).view(N, r)
+    locs = (-10 + 20*torch.rand(N, dtype=torch.float32)).repeat_interleave(r).view(N, r)
+    scales = (0.1 + 5*torch.rand(N, dtype=torch.float32)).repeat_interleave(r).view(N, r)
 
     C = torch.stack((locs, scales), dim = 2)
 
     locs2D = locs.unsqueeze(-1).expand(-1, -1, d)
     scales2D = scales.unsqueeze(-1).expand(-1, -1, d)
+
+    
 
     Y = torch.tensor(stats.norm.rvs(loc=locs2D, scale=scales2D, size=(N, r, d)), dtype=torch.float32)
 
